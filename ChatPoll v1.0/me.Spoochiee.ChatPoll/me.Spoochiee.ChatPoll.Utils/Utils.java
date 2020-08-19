62f387c6cd7269a99bf4df8a37940662e18d3c2b
Spoochiee <68448431+Spoochiee@users.noreply.github.com>
2020-08-19 23:24:14 +0100

    Utils.java

---
+++ ChatPoll v1.0/me.Spoochiee.ChatPoll/me.Spoochiee.ChatPoll.Utils/Utils.java
@@ -0,0 +1,10 @@
+package me.Spoochiee.ChatPoll.Utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+	
+	public String chat(String string) {
+		return ChatColor.translateAlternateColorCodes('&', string);
+	}
+}
